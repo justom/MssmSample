8cfa353980c6912f1522abe0a522018f77691239
Tom Giordano <justom@gmail.com>
2014-04-22 12:18:48 -0400

    Implement is_valid_cycle(), improve logging.  Decide case-insensitive is okay.

--- list_sample_dirs.py
+++ list_sample_dirs.py
@@ -25,8 +25,8 @@
 #    final one as the cycle name if it's a valid strong.
 # 3. Don't worry about validating the date as long as it's a valid number.
 
-import logging
-logging.basicConfig(level=logging.WARNING)
+import logging, re
+logging.basicConfig(level=logging.DEBUG)
 
 def list_sample_dirs(dir):
     '''Given a directory, list all subdirectories (depth 1) that contain samples.'''
@@ -48,20 +48,27 @@ def is_valid_date(str):
 
 def is_valid_cycle(str):
     '''A valid cycle string is of the form CXXX where X are digits.'''
-    # match C\d{3}
-    # make an assumption about 'c' vs 'C'
-    logging.warning("Unimplemented.")
-    return False
+    if len(str) != 4:
+        return False
+    # match C\d{3} or c\d{3}
+    # Match case-insensitive to be slightly liberal in what we accept.
+    if None == re.match('C\d{3}', str, re.I):
+        return False
+
+    return True
 
 def test_is_valid_cycle():
-    bad_cycles = ["", "0", "00", "00x", "1234", "C1234", "Cabc", 1234]
+    logging.info("Test is_valid_cycle()")
+    bad_cycles = ["", "0", "00", "00x", "1234", "C1234", "Cabc", " c123"]
     for each in bad_cycles:
+        logging.debug( "invalid cycle: " + each)
         assert not is_valid_cycle(each)
-    good_cycles = ["C000", "C999", "C123"]
+    good_cycles = ["C000", "C999", "C123", "c456"]
     for each in good_cycles:
         assert is_valid_cycle(each)
 
 def test_is_valid_date():
+    logging.info( "Test is_valid_date()")
     bad_dates = ["", "0", "00", "00x", "1234", "C1234", "Cabc", 123456, "12354"]
     for each in bad_dates:
         assert not is_valid_date(each)
@@ -70,6 +77,7 @@ def test_is_valid_date():
         assert is_valid_date(each)
 
 def test_parse_cycle_number():
+    logging.info( "Test parse_cycle_number")
     invalid_subdirs = ["", "foobar", "a miscellaneous string", "bad prefix_C234_000000", "C002_130905", "PT000_Normal_C002__130905", "LA_C00_130905", "LA_001_130905"]
 
     for each in invalid_subdirs:
@@ -80,10 +88,10 @@ def test_parse_cycle_number():
         assert valid_subdirs[each] == parse_cycle_number(each)
 
 def test_list_sample_dirs():
-    print "Test on some expected directories that have no samples."
+    logging.info( "Test list_sample_dirs() on some expected directories that have no samples.")
     list_sample_dirs("/var")
     list_sample_dirs(".")
-    print "Test on the directory structure provided for this project.  MAX should be next to 13006_C200_130912."
+    logging.info( "Test on the directory structure provided for this project.  MAX should be next to 13006_C200_130912.")
     list_sample_dirs("q1_test_data")
     
 if __name__ == "__main__":
