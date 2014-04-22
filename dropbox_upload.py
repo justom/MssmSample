fa3b4ea6c72999f713dd955e9b6b8df1f3bca4ca
Tom Giordano <justom@gmail.com>
2014-04-22 16:24:16 -0400

    Parse CSV test and implementation.

--- dropbox_upload.py
+++ dropbox_upload.py
@@ -27,8 +27,10 @@
 APP_KEY = 'leikm3a4vbv3h9n'
 APP_SECRET = 'gbwce6od65g3f9g'
 
-import sys, logging
+import sys, logging, os
 from dropbox import client, rest, session
+
+logging.basicConfig(level=logging.DEBUG)
 
 def upload_file_to_dropbox(src, dest):
     '''Move the file specified in src to the destination specified on dropbox.'''
@@ -48,39 +50,64 @@ def parse_csv_file(filename):
     '''Given a csv file, parse it into a collection of pairs of source
     files and their destinations.  For bad entries in the CSV file,
     log a warning and continue, rather than aborting.'''
-    logging.warning("parse_csv_file: Unimplemented.")
-    return None
+    results = []
+    if not os.path.exists(filename):
+        logging.warning(filename + ': file does not exist.')
+        return results
+    if not os.path.isfile(filename):
+        logging.warning(filename + ': not a file.')
+        return results
+
+    csv_file = open(os.path.expanduser(filename), "r")
+    for line in csv_file:
+        components = line.strip().split(',')
+        if len(components) != 2:
+            logging.warning('Invalid line, skipping: ' + line)
+        else:
+            results.append([components[0],components[1]])
+            logging.debug('Valid: ' + components[0] + ',' + components[1])
+        
+    return results
 
 def test_parse_csv():
+    directory_name = "q2_test_data"
     nonexistent_file = "q2_test_data/dne.csv"
     empty_file = "q2_test_data/empty.csv"
     invalid_file = "q2_test_data/invalid.txt"
     mixed_file = "q2_test_data/mixed.csv"
     good_file = "q2_test_data/files.csv"
-    
-    assert None == parse_csv_file(nonexistent_file)
-    assert None == parse_csv_file(empty_file)
-    assert None == parse_csv_file(invalid_file)
+
+    result = parse_csv_file('')
+    assert len(result) == 0
+    result = parse_csv_file(nonexistent_file)
+    assert len(result) == 0
+    result = parse_csv_file(directory_name)
+    assert len(result) == 0
+    result = parse_csv_file(empty_file)
+    assert len(result) == 0
+    result = parse_csv_file(invalid_file)
+    assert len(result) == 0
 
     # the parse function should only ensure that the file format is
     # valid, not whether the contents specify files that exist.  The
     # uploading function will validate that the files exist before
     # processing them.
-    result = parse_csv_file(mixed.csv)
-    assert len(result) == 4
+    result = parse_csv_file(mixed_file)
+    assert len(result) == 5
     assert result[0] == ['./q2_test_data/file1', 'files/file1.txt']
     assert result[1] == ['./dne.csv', 'files/file1.txt']
     assert result[2] == ['./q2_test_data/file2','special_name.txt']
-    assert result[3] == ['./q2_test_data/file2','another_place.txt']
+    assert result[3] == ['miscellaneous text','files/file1.txt']
+    assert result[4] == ['./q2_test_data/file2','another_place.txt']
 
-    result = parse_csv_file(good.csv)
+    result = parse_csv_file(good_file)
     assert len(result) == 2
     assert result[0] == ['./q2_test_data/file1', 'files/file1.txt']
     assert result[1] == ['./q2_test_data/file2','special_name.txt']
 
 def main(csv_file):
     '''Parse the CSV file and then for each result attempt to upload the file to dropbox.'''
-    print csv_file
+    test_parse_csv()
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
